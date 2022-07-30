2aa00c3e8bddd98f9d6c7aefda64e3c956eabe53
eduardonot <eduardo_not@hotmail.com>
2022-07-29 22:23:18 -0300

    feat: :sparkles: add data table header

---
+++ client/src/components/HeaderContainer.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app-bar
+    flat
+    color="rgba(0, 0, 0, 0)"
+  >
+    <v-toolbar-title>{{title}}</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn v-if="title === 'Meus Livros'" @click="addNew">
+      Adicionar Novo
+    </v-btn>
+  </v-app-bar>
+</template>
+<script>
+export default {
+  name: 'HeaderContainer',
+  computed: {
+    title () {
+      return this.$store.getters['structure/getAppTitle']
+    }
+  },
+  methods: {
+    addNew () {
+      this.$store.commit('structure/openPreviewNewBookModal')
+    }
+  }
+}
+</script>
